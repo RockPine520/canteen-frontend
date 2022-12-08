57f8b22a7c308ca52d75153506e79eba08108746
RockPine520 <47073688+RockPine520@users.noreply.github.com>
2022-12-08 14:51:48 +0800

    新增打卡规则 修改逻辑

--- src/views/Sign.vue
+++ src/views/Sign.vue
@@ -84,19 +84,19 @@
         width="60%"
         :before-close="addRuleClose"
     >
-      <el-form :model="form" :rules="rules" ref="addFormRef" label-width="70px" :label-position='labelPosition'>
+      <el-form :model="form" :rules="rules" ref="addFormRef" label-width="80px" :label-position='labelPosition'>
         <el-form-item label="规则名称" prop="ruleName">
           <el-input v-model="form.ruleName" style="width: 90%"/>
         </el-form-item>
-        <el-form-item label="考勤时间" prop="weekDay">
-          <el-button type="primary" @click="timeDialogOpen">
+        <el-form-item label="考勤时间">
+          <el-button type="primary" @click="timeDialogOpen" :disabled="timeButtonAble">
             <el-icon style="margin-right:3px">
               <CirclePlusFilled/>
             </el-icon>
             新增
           </el-button>
         </el-form-item>
-        <el-form-item v-for="(oneWeekDay,index) in form.weekday.slice(1)" :key="index"
+        <el-form-item v-for="(oneWeekDay,index) in form.weekday" :key="index"
                       style="margin-bottom: 10px">
           工作日:
           <el-tag v-for="(oneDay,oIndex) in oneWeekDay.day" :key="oIndex" style="margin: 0 5px">
@@ -104,16 +104,16 @@
           </el-tag>
           打卡时间:
           <el-tag v-for="(validTime,vIndex) in oneWeekDay.validCycle" :key="vIndex" style="margin: 0 5px">
-            {{ validTime.startTime }} — {{ validTime.endTime }}
+            {{ validTime.startTime }} — {{ validTime.endTime }} 【{{dutyDict[validTime.isOnDuty]}}】
           </el-tag>
-          <el-button size="small" type="primary" @click="editSignDay(index+1)" style="margin-left: 5px">修改</el-button>
-          <el-button size="small" type="danger" @click="deleteSignDay(index+1)">删除</el-button>
+          <el-button size="small" type="primary" @click="editSignDay(index)" style="margin-left: 5px">修改</el-button>
+          <el-button size="small" type="danger" @click="deleteSignDay(index)">删除</el-button>
         </el-form-item>
       </el-form>
       <template #footer>
       <span class="dialog-footer">
         <el-button @click="addRuleClose">取消</el-button>
-        <el-button type="primary" @click="addSave('addFormRef')">
+        <el-button type="primary" @click="addRuleSave('addFormRef')">
           保存
         </el-button>
       </span>
@@ -123,13 +123,13 @@
     <el-dialog
         v-model="setSignTimeVisible"
         title="设置打卡时间"
-        width="60%"
+        width="50%"
         :before-close="timeClose"
     >
       <!--      注：weekday这个字段实现了双向绑定-->
-      <el-form :model="form.weekday[weekIndex]" ref="timeFormRef" label-width="70px" label-position="top">
+      <el-form :model="setTimeForm" ref="timeFormRef" label-width="70px" label-position="top">
         <el-form-item label="工作日" prop="day" :rules="timeRules.day">
-          <el-checkbox-group v-model="form.weekday[weekIndex].day">
+          <el-checkbox-group v-model="setTimeForm.day">
             <el-checkbox :label="1" name="day" :disabled="checkList[0]">周一</el-checkbox>
             <el-checkbox :label="2" name="day" :disabled="checkList[1]">周二</el-checkbox>
             <el-checkbox :label="3" name="day" :disabled="checkList[2]">周三</el-checkbox>
@@ -140,20 +140,32 @@
           </el-checkbox-group>
         </el-form-item>
         <el-form-item label="打卡时间">
-          <el-form-item v-for="(validTime,index) in form.weekday[weekIndex].validCycle" :key="validTime.key">
+          <el-form-item v-for="(validTime,index) in setTimeForm.validCycle" :key="validTime.key">
             <div :class="index===0?'normal':'mgt10px'">
               <el-tag>
                 时间段{{ index + 1 }}
               </el-tag>
               <el-form-item :prop="`validCycle.${index}.startTime`" :rules="timeRules.startTime"
                             style="margin-left: 10px">
-                <el-time-select :disabled="index!==nIndex" v-model="validTime.startTime" placeholder="起始时间"
+                <el-time-select :disabled="index!==nIndex" v-model="validTime.startTime" placeholder="开始时间"
                                 :start="sTime" :step="timeStep" :end="eTime"
                                 :max-time="validTime.endTime"></el-time-select>
               </el-form-item>
               <el-form-item :prop="`validCycle.${index}.endTime`" :rules="timeRules.endTime" style="margin-left: 10px">
                 <el-time-select :disabled="index!==nIndex" v-model="validTime.endTime" placeholder="结束时间" :start="sTime"
                                 :step="timeStep" :end="eTime" :min-time="validTime.startTime"></el-time-select>
+              </el-form-item>
+              <el-form-item :prop="`validCycle.${index}.isOnDuty`" :rules="timeRules.isOnDuty"
+                            style="margin-left: 10px">
+                <el-select :disabled="index!==nIndex" v-model="validTime.isOnDuty" placeholder="打卡类型">
+                  <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  >
+                  </el-option>
+                </el-select>
               </el-form-item>
               <el-button :disabled="index!==nIndex || validTime.startTime.length===0 || validTime.endTime.length===0"
                          type="primary" @click.prevent="addValidTime(validTime.endTime,index)"
@@ -180,13 +192,13 @@
     <el-dialog
         v-model="editSignTimeVisible"
         title="修改打卡时间"
-        width="60%"
+        width="50%"
         :before-close="editTimeClose"
     >
       <!--      注：weekday这个字段实现了双向绑定-->
-      <el-form :model="form.weekday[editWeekIndex]" ref="editTimeFormRef" label-width="70px" label-position="top">
+      <el-form :model="editTimeForm" ref="editTimeFormRef" label-width="70px" label-position="top">
         <el-form-item label="工作日" prop="day" :rules="timeRules.day">
-          <el-checkbox-group v-model="form.weekday[editWeekIndex].day">
+          <el-checkbox-group v-model="editTimeForm.day">
             <el-checkbox :label="1" name="day" :disabled="checkList[0]">周一</el-checkbox>
             <el-checkbox :label="2" name="day" :disabled="checkList[1]">周二</el-checkbox>
             <el-checkbox :label="3" name="day" :disabled="checkList[2]">周三</el-checkbox>
@@ -197,7 +209,7 @@
           </el-checkbox-group>
         </el-form-item>
         <el-form-item label="打卡时间">
-          <el-form-item v-for="(validTime,index) in form.weekday[editWeekIndex].validCycle" :key="validTime.key">
+          <el-form-item v-for="(validTime,index) in editTimeForm.validCycle" :key="validTime.key">
             <div :class="index===0?'normal':'mgt10px'">
               <el-tag>
                 时间段{{ index + 1 }}
@@ -211,6 +223,18 @@
               <el-form-item :prop="`validCycle.${index}.endTime`" :rules="timeRules.endTime" style="margin-left: 10px">
                 <el-time-select :disabled="index!==nIndex" v-model="validTime.endTime" placeholder="结束时间" :start="sTime"
                                 :step="timeStep" :end="eTime" :min-time="validTime.startTime"></el-time-select>
+              </el-form-item>
+              <el-form-item :prop="`validCycle.${index}.isOnDuty`" :rules="timeRules.isOnDuty"
+                            style="margin-left: 10px">
+                <el-select :disabled="index!==nIndex" v-model="validTime.isOnDuty" placeholder="打卡类型" style="width: 100px">
+                  <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                  >
+                  </el-option>
+                </el-select>
               </el-form-item>
               <el-button :disabled="index!==nIndex || validTime.startTime.length===0 || validTime.endTime.length===0"
                          type="primary" @click.prevent="editAddValidTime(validTime.endTime,index)"
@@ -233,6 +257,7 @@
       </template>
     </el-dialog>
 
+    <!--    编辑规则（没写呢）-->
     <el-dialog
         v-model="editDialogVisible"
         title="编辑规则"
@@ -267,8 +292,8 @@ export default {
     CirclePlusFilled, Promotion, DeleteFilled, EditPen, CirclePlus, Delete
   },
   data() {
-
     return {
+      timeButtonAble: false,
       tableData: [],
       addDialogVisible: false,
       editDialogVisible: false,
@@ -278,31 +303,9 @@ export default {
         ruleId: "",
         ruleName: "",
         version: 1,
-        weekday: [{
-          day: [],
-          validCycle: [{
-            startTime: '',
-            endTime: '',
-            key: ''
-          }]
-        }],
-        passable: [{
-          start: '',
-          end: '',
-          validCycle: [{
-            startTime: '',
-            endTime: ''
-          }]
-        }],
-        impassable: [{
-          start: '',
-          end: '',
-          validCycle: [{
-            startTime: '',
-            endTime: ''
-          }]
-        }],
       },
+      setTimeForm: {},
+      editTimeForm: {},
       addTimeAble: true,
       sTime: '03:00',
       eTime: '22:00',
@@ -310,9 +313,12 @@ export default {
       nIndex: 0,
       labelPosition: 'right',
       rules: {
-        depName: [
-          {required: true, message: "请输入部门名称", trigger: 'blur'},
+        ruleName: [
+          {required: true, message: "请输入规则名称", trigger: 'blur'},
         ],
+        weekday: [
+          {required: true, message: "请添加考勤时间", trigger: 'change'}
+        ]
       },
       timeRules: {
         startTime: [
@@ -338,8 +344,8 @@ export default {
       disabled: false,
       search: '',
       aWeekdays: [],
-      weekIndex: 0, // weekday数组的下标
-      editWeekIndex: 0, // 编辑时weekday数组下标
+      // weekIndex: 0, // weekday数组的下标
+      // editWeekIndex: 0, // 编辑时weekday数组下标
       checkList: [false, false, false, false, false, false, false],
       weekDict: {
         1: "周一",
@@ -350,15 +356,26 @@ export default {
         6: "周六",
         7: "周日"
       },
+      dutyDict:{
+        true:'签到',
+        false:'签退'
+      },
       tempArray: undefined,
       tempCheck: undefined,
+      options: [{
+        value: true,
+        label: '签到'
+      }, {
+        value: false,
+        label: '签退'
+      }]
     }
   },
 
   methods: {
     addRuleDialogOpen() {
       this.addDialogVisible = true
-      this.weekIndex = 0
+      this.timeButtonAble = false
       this.editWeekIndex = 0
       this.checkList = [false, false, false, false, false, false, false]
       this.tempArray = undefined
@@ -371,58 +388,41 @@ export default {
         ruleId: "",
         ruleName: "",
         version: 1,
-        weekday: [{
-          day: [],
-          validCycle: [{
-            startTime: '',
-            endTime: '',
-            key: ''
-          }]
-        }],
-        passable: [{
-          start: '',
-          end: '',
-          validCycle: [{
-            startTime: '',
-            endTime: ''
-          }]
-        }],
-        impassable: [{
-          start: '',
-          end: '',
-          validCycle: [{
-            startTime: '',
-            endTime: ''
-          }]
-        }],
+        weekday: [],
+        passable: [],
+        impassable: [],
       }
     },
-    editSignDay(index) {
-      this.editSignTimeVisible = true
-      this.editWeekIndex = index
-      // index表示weekday下标
-      this.tempArray = JSON.parse(JSON.stringify(this.form.weekday[this.editWeekIndex]))
-      this.tempCheck = JSON.parse(JSON.stringify(this.checkList))
-      console.log("before", this.tempArray)
-      for (let i of this.form.weekday[this.editWeekIndex].day) {
-        this.checkList[i - 1] = false
-      }
-      this.nIndex = this.form.weekday[this.editWeekIndex].validCycle.length - 1
-      let len = this.form.weekday[this.editWeekIndex].validCycle.length
-      this.sTime = len - 2 >= 0 ? this.form.weekday[this.editWeekIndex].validCycle[len - 2].endTime : "03:00"
-    },
-    deleteSignDay(index) {
-      for (let i of this.form.weekday[index].day) {
-        this.checkList[i - 1] = false
-      }
-      if (this.editWeekIndex >= index) {
-        this.editWeekIndex = this.editWeekIndex - 1
-      }
-      this.form.weekday.splice(index, 1)
-      this.weekIndex = this.weekIndex - 1 // 这个指向数组最后一位
+
+    timeDialogOpen() {
+      this.setSignTimeVisible = true
+      // this.form['weekday'] = []
+      // this.form['weekday'].push({
+      //   day: [],
+      //   // validCycle: [{
+      //   //   startTime: '',
+      //   //   endTime: '',
+      //   //   key: ''
+      //   // }]
+      // })
+      // setTimeForm as 'weekday' array
+      this.setTimeForm['day'] = [] // 存int
+      this.setTimeForm['validCycle'] = [{
+        startTime: '',
+        endTime: '',
+        isOnDuty: '',
+        key: Date.now()
+      }] // 存对象
+
+      this.nIndex = 0
+      this.$nextTick(() => {
+        this.$refs.timeFormRef.resetFields()
+      })
+      console.log("this.form.weekday", this.form.weekday)
+      console.log("this.setTimeForm", this.setTimeForm)
     },
     addValidTime(cutTime, index) {
-      this.form.weekday[this.weekIndex].validCycle.push({
+      this.setTimeForm.validCycle.push({
         startTime: '',
         endTime: '',
         key: Date.now()
@@ -433,14 +433,78 @@ export default {
       }
     },
     removeValidTime(item) {
-      let index = this.form.weekday[this.weekIndex].validCycle.indexOf(item)
+      let index = this.setTimeForm.validCycle.indexOf(item)
       if (index !== 0) {
-        this.form.weekday[this.weekIndex].validCycle.splice(index, 1)
+        this.setTimeForm.validCycle.splice(index, 1)
         this.nIndex = index - 1
       }
     },
+    timeDialogSave(forName) {
+      console.log("setTimeForm", this.setTimeForm)
+      this.$refs[forName].validate((valid) => {
+        if (valid) {
+          this.setSignTimeVisible = false
+          this.setTimeForm.day.sort()
+          for (let index of this.setTimeForm.day) {
+            this.checkList[index - 1] = true
+          }
+          let tmp = JSON.parse(JSON.stringify(this.setTimeForm))
+          this.form.weekday.push(tmp)
+          this.timeButtonAble = !this.checkList.includes(false);
+        }
+      })
+    },
+    timeClose() {
+      // let index = this.form.weekday.length - 1
+      // this.form.weekday.splice(index, 1)
+      this.nIndex = 0
+      this.setSignTimeVisible = false
+      // this.$refs.timeFormRef.resetFields()
+    },
+    editSignDay(index) {
+      this.editSignTimeVisible = true
+      this.editWeekIndex = index
+      this.editTimeForm = JSON.parse(JSON.stringify(this.form.weekday[index]))  // 深拷贝
+      // index表示weekday下标
+      this.tempArray = JSON.parse(JSON.stringify(this.editTimeForm))
+      this.tempCheck = JSON.parse(JSON.stringify(this.checkList))
+      console.log("before", this.tempArray)
+      for (let i of this.editTimeForm.day) {
+        this.checkList[i - 1] = false
+      }
+      this.nIndex = this.editTimeForm.validCycle.length - 1
+      let len = this.editTimeForm.validCycle.length
+      this.sTime = len - 2 >= 0 ? this.editTimeForm.validCycle[len - 2].endTime : "03:00"
+    },
+    deleteSignDay(index) {
+      for (let i of this.form.weekday[index].day) {
+        this.checkList[i - 1] = false
+      }
+      if (this.editWeekIndex > index) {
+        this.editWeekIndex = this.editWeekIndex - 1
+      }
+      this.form.weekday.splice(index, 1)
+      console.log("delete", this.form)
+      this.timeButtonAble = !this.checkList.includes(false);
+      // this.weekIndex = this.weekIndex - 1 // 这个指向数组最后一位
+    },
+    editTimeDialogSave(forName) {
+      this.$refs[forName].validate((valid) => {
+        if (valid) {
+          this.editSignTimeVisible = false
+          this.editTimeForm.day.sort()
+          // this.checkList=[false, false, false, false, false, false, false]
+          for (let index of this.editTimeForm.day) {
+            this.checkList[index - 1] = true
+          }
+          this.form.weekday[this.editWeekIndex] = JSON.parse(JSON.stringify(this.editTimeForm))
+          this.nIndex = 0
+          this.timeButtonAble = !this.checkList.includes(false);
+        }
+      })
+    },
     editAddValidTime(cutTime, index) {
-      this.form.weekday[this.editWeekIndex].validCycle.push({
+      this.editTimeForm.validCycle.push({
         startTime: '',
         endTime: '',
         key: Date.now()
@@ -451,11 +515,19 @@ export default {
       }
     },
     editRemoveValidTime(item) {
-      let index = this.form.weekday[this.editWeekIndex].validCycle.indexOf(item)
+      let index = this.editTimeForm.validCycle.indexOf(item)
       if (index !== 0) {
-        this.form.weekday[this.editWeekIndex].validCycle.splice(index, 1)
+        this.editTimeForm.validCycle.splice(index, 1)
         this.nIndex = index - 1
       }
+    },
+    editTimeClose() {
+      this.editSignTimeVisible = false
+      // this.$refs.timeFormRef.resetFields()
+      this.checkList = this.tempCheck
+      // this.form.weekday[this.editWeekIndex] = this.tempArray
+      console.log("nowwhat", this.form.weekday)
+      this.editWeekIndex = 0
     },
     loadData() {
       this.loading = true
@@ -520,40 +592,6 @@ export default {
       this.editDialogVisible = false
       this.$refs.editFormRef.resetFields()
     },
-    editTimeClose() {
-      this.editSignTimeVisible = false
-      // this.$refs.timeFormRef.resetFields()
-      this.checkList = this.tempCheck
-      this.form.weekday[this.editWeekIndex] = this.tempArray
-      console.log("nowwhat", this.form.weekday)
-      this.editWeekIndex = 0
-    },
-    timeClose() {
-      let index = this.form.weekday.length - 1
-      this.form.weekday.splice(index, 1)
-      this.nIndex = 0
-      this.weekIndex = index - 1
-      this.setSignTimeVisible = false
-      // this.$refs.timeFormRef.resetFields()
-    },
-    timeDialogOpen() {
-      this.setSignTimeVisible = true
-      this.form.weekday.push({
-        day: [],
-        validCycle: [{
-          startTime: '',
-          endTime: '',
-          key: ''
-        }]
-      })
-      this.weekIndex = this.form.weekday.length - 1
-      this.nIndex = 0
-      this.$nextTick(() => {
-        this.$refs.timeFormRef.resetFields()
-      })
-      console.log("weekIndex", this.weekIndex)
-      console.log("this.form.weekday", this.form.weekday)
-    },
     addDialogClose(formName) {
       this.form = {}
       this.addDialogVisible = false
@@ -564,35 +602,12 @@ export default {
       this.editDialogVisible = false
       this.$refs[formName].resetFields()
     },
-    editTimeDialogSave(forName) {
-      this.$refs[forName].validate((valid) => {
-        if (valid) {
-          this.editSignTimeVisible = false
-          this.form.weekday[this.editWeekIndex].day.sort()
-          // this.checkList=[false, false, false, false, false, false, false]
-          for (let index of this.form.weekday[this.editWeekIndex].day) {
-            this.checkList[index - 1] = true
-          }
-          this.nIndex = 0
-        }
-      })
-    },
-    timeDialogSave(forName) {
-      this.$refs[forName].validate((valid) => {
-        if (valid) {
-          this.setSignTimeVisible = false
-          this.form.weekday[this.weekIndex].day.sort()
-          for (let index of this.form.weekday[this.weekIndex].day) {
-            this.checkList[index - 1] = true
-          }
-        }
-      })
 
-    },
-    async addSave(formName) {
+
+    async addRuleSave(formName) {
       await this.$refs[formName].validate((valid) => {
         if (valid) {
-          console.log("调用新增部门接口")
+          console.log("调用新增规则接口")
           request.post('/department/addDep', this.form).then(res => {
             console.log("addDep", res)
             if (res["code"] === 1) {
